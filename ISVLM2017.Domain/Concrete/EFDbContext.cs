ba5824c76b79db2830bce384f9c3909b3036a621
Hacama <skanpollo@gmail.com>
2018-01-03 21:47:58 -0500

    Revert "si"
    This reverts commit c1c1247ff0d536998ed0d2f3789c55c21fda2b74.

--- ISVLM2017.Domain/Concrete/EFDbContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using ISVLM2017.Domain.Entities;
-using System.Data.Entity;
-namespace ISVLM2017.Domain.Concrete
-{
-    class EFDbContext: DbContext
-    {
-        public DbSet<CajaDetalle> CajaDetalles { get; set; }
-    }
-}
--- ISVLM2017.WebUI/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ISVLM2017.WebUI.MvcApplication" Language="C#" %>
